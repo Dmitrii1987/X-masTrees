1b071f94e5beb06599c87633f1fbe8e4a48accfb
Dmitrii_K <dmitrii.kuzminyh@gmail.com>
2023-02-25 22:09:58 +0300

    feat: write script in timer.js

---
+++ script/timer.js
@@ -0,0 +1,14 @@
+const timer = document.createElement('div');
+const timerText = document.createElement('p');
+const timerCount = document.createElement('span');
+
+timer.classList.add('timer');
+timerText.classList.add('timer__text');
+timerCount.classList.add('timer__count');
+
+timerText.textContent = 'До конца акции осталось:';
+timerCount.textContent = '3 дня 12 часов 10 минут 5 секунд';
+
+timerText.append(timerCount);
+timer.append(timerText);
+document.body.append(timer);
