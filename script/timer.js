0c2cb339868223ae1394c7ab10211f8a9e4a8da7
Dmitrii_K <dmitrii.kuzminyh@gmail.com>
2023-02-28 17:52:38 +0300

    feat: update timer.js

--- script/timer.js
+++ script/timer.js
@@ -1,3 +1,7 @@
+const declOfNum = (n, titles) => titles[n % 10 === 1 && n % 100 !== 11 ?
+    0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+
+
 const timer = () => {
     const timer = document.createElement('div');
     const timerText = document.createElement('p');
@@ -8,19 +12,30 @@ const timer = () => {
     timerCount.classList.add('timer__count');
 
     timerText.textContent = 'До конца распродажи осталось: ';
-    timerCount.textContent = '3 дня 12 часов 10 минут 5 секунд';
+    
 
     timerText.append(timerCount);
     timer.append(timerText);
     document.body.prepend(timer);
 
     const startTimer = () => {
-        const deadline = new Date(2023, 1, 25, 23, 33, 0);
+        const deadline = new Date(2023, 2, 28, 23, 33, 0);
         const now = new Date();
-        const timeRemaining = deadline - now;
-        console.log('timeRemaining: ', timeRemaining);
+        const timeRemaining = (deadline - now) / 1000;
+
+        const seconds = Math.floor(timeRemaining % 60);
+        const minutes = Math.floor(timeRemaining / 60 % 60);
+        const hours = Math.floor(timeRemaining / 60 / 60 % 24);
+        const days = Math.floor(timeRemaining / 60 / 60 / 24 % 30);
+        console.log('days: ', days, 'hours: ', hours, 'minutes: ', minutes, 'seconds: ', seconds);
+
+        declOfNum(seconds, ['секунда', 'секунды', 'секунд']);
+        declOfNum(minutes, ['минута', 'минуты', 'минут']);
+        declOfNum(hours, ['час', 'часа', 'часов']);
+        declOfNum(days, ['день', 'дня', 'дней']);
 
 
+        timerCount.textContent = `${days} дня ${hours} часов ${minutes} минут ${seconds} секунд`
 
         if (timeRemaining > 0) {
             setTimeout(startTimer, 1000)
